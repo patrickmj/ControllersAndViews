fa8480f7c19632de3068d8574164f1a2517aa64e
patrickmj <patrickmjchnm@gmail.com>
2013-06-04 12:49:44 -0400

    Adds an additional layer to demo urls and directories/files more fully.

---
+++ controllers/WhatnotController.php
@@ -0,0 +1,24 @@
+<?php
+
+class ControllersAndViews_WhatnotController extends Omeka_Controller_AbstractActionController
+{
+    public function init()
+    {
+        //in real life, this would be the model for the plugin
+        $this->_helper->db->setDefaultModelName('Item');
+    }
+    
+    public function browseAction()
+    {
+        $this->view->data = array('Fake data 1', 'Fake data 2', 'Fake data 3');
+        $records = $this->_helper->db->getTable()->findBy(array('public'=>true));
+        $this->view->items = $records;
+    }    
+    
+    public function thingyAction()
+    {
+        $this->view->thingy = "Just some text.";
+    }
+    
+    
+}
---
+++ views/public/whatnot/thingy.php
@@ -0,0 +1,12 @@
+<?php
+echo head(array('title'=>"Just a fake title. Usually comes from real data"));
+?>
+
+<h1><?php echo "Some fake title"; ?></h1>
+
+<p>Notice how the url path corresponds to the views directory structure</p>
+<p><?php echo $thingy; ?></p>
+
+<?php 
+echo foot();
+?>
